72938383b4477fb4043908213dc2c1ea987dd88b
dwmichalak <dwmichalak@cpp.edu>
2023-05-17 20:15:55 -0700

    feat: add Quick Select & MM Quick Select algos
    implemented those algorithms and code to
    demonstrate test cases in the main method

--- Project2Main.java
+++ Project2Main.java
@@ -26,11 +26,62 @@ public class Project2Main {
 
         // MAIN MENU LOOP
         while(keepRunning) {
-            System.out.print("\n(1) Select test cases to display\n(2) Generate 2^k length random integer arrays"
-            + "\n(3) Test Algorithm Speeds\n(0) Exit");
+            System.out.print("(1) Select test cases to display\n(2) Generate 2^k length random integer arrays"
+            + "\n(3) Test Algorithm Speeds\n(0) Exit\n");
             int inpChoice = input.nextInt();
 
             if (inpChoice == 1) {
+                //define a List to store all the entries in testCases.txt
+                boolean keepTesting = true;
+                List<int[]> caseList = new ArrayList<>();
+
+                /*
+                 * read testCases.txt file within this program's directory, store entires as arrays 
+                 * of integers in the arrayList, caseList
+                 * CATCH: a fileNotFoundException, if testCases.txt does not exist
+                */
+                Scanner fileReader = new Scanner(new File("./testCases.txt"));
+
+                /*
+                    * TAKE TEST CASE FILE AND PARSE EACH CASE INTO AN ArrayList ENTRY
+                    */
+                while(fileReader.hasNext()) {
+
+                    String[] testCaseRaw =  fileReader.nextLine().split(":");
+                    int[] testCaseInt = new int[testCaseRaw.length];
+
+                    for (int i = 0; i < testCaseRaw.length; i++) {
+                        testCaseInt[i] = Integer.parseInt(testCaseRaw[i]);
+                    }
+
+                    caseList.add(testCaseInt);
+                }
+                fileReader.close();
+
+
+                /*
+                * USER MENU FOR VIEWING INDIVIDUAL TEST CASES
+                */
+                while(keepTesting) {
+
+                    /*
+                     * Ask the user what test case they want to display
+                     * input: the test case #
+                     */
+                    System.out.println("(1-10)Select a test case\n(0)Exit");
+                    int caseChoice = input.nextInt();
+                    if (caseChoice > 0 && caseChoice < 11) {
+
+                        //process user input to correctly align with ArrayList indexes
+                        caseChoice--;
+
+                        //declare 3 square matrices to use for the multiplicands and their product, as well as set variables for organizing the data in 2d arrays
+                        int[] chosenCase = caseList.get(caseChoice);
+
+                        printTestCase(Arrays.copyOfRange(chosenCase, 1, chosenCase.length), chosenCase[0]);
+                    } else
+                        keepTesting = false;
+                }
 
             } else if (inpChoice ==2) {
 
@@ -47,11 +98,24 @@ public class Project2Main {
     }
 
     private static void printArray(int[] array) {
-
+        // loop through the array, printing each element between brackets
+        System.out.print("[ ");
+        for (int i = 0; i < array.length; ++i) {
+            System.out.print(array[i] + " ");
+        }
+        System.out.print("]");
     }
 
     private static void printTestCase(int[] array, int k) {
+        System.out.printf("Element %d of the following array, once sorted : \n", k);
+        printArray(array);
 
+        int a, b, c;
+        a = SelectionAlgorithms.mergeSortSelect(array, k);
+        b = SelectionAlgorithms.quickSelect(array, k);
+        c = SelectionAlgorithms.quickSelectMM(array, k);
+        
+        System.out.printf("\nMerge Sort: %d\nQuick Select: %d\nMedian of Medians: %d\n", a, b, c);
     }
 
     private static void printTimeResults(int[] array) {
@@ -67,7 +131,7 @@ public class Project2Main {
         return temp;
     }
 
-    private static double[] testAlgos(int[] array, int k) {
+    private static void testAlgos(int[] array, int k) {
         // initialize and define array of execution times
         double[] returnTimes = new double[3];
 
--- SelectionAlgorithms.java
+++ SelectionAlgorithms.java
@@ -1,12 +1,17 @@
 import java.util.Arrays;
+import java.lang.Math;
 
 public class SelectionAlgorithms {
 
 
-    // Merge Select sorts the array in ascending order with a Merge Sort algorithm, then retrives the kth item.
-
+    /**
+     * Sorts the array in ascending order with a Merge Sort algorithm, then retrives the kth smallest item.
+     * @param input the array to be searched
+     * @param entry the index of the item to be returned
+     * @return the kth item
+     */
     public static int mergeSortSelect(int[] input, int entry) {
-        return mergeSort(input)[entry];
+        return mergeSort(input)[entry-1];
     }
 
     private static int[] mergeSort(int[] input) {
@@ -57,4 +62,128 @@ public class SelectionAlgorithms {
         // return merged(sorted) array
         return merged;
     }
+
+    /**
+     * Uses a quick sort algorithm to find the kth smallest entry in an array
+     * @param array the array to be operated on
+     * @param entry the index of the item to be returned
+     * @return the kth item
+     */
+    public static int quickSelect(int[] array, int entry) {
+        return quickSelect(array, 0, array.length - 1, entry-1);
+    }
+
+    private static int quickSelect(int[] array, int left, int right, int entry) {
+        if (left == right)
+            return array[left];
+        
+        int pivotIndex = partition(array, left, right);
+
+        if (entry == pivotIndex)
+            return array[entry];
+        else if (entry < pivotIndex) 
+            return quickSelect(array, left, pivotIndex - 1, entry);
+        else 
+            return quickSelect(array, pivotIndex + 1, right, entry);
+    }
+
+    private static int partition(int[] array, int left, int right) {
+        int pivot = array[left];
+        int i = left + 1, j = right;
+
+        while (i <= j) {
+            while (i <= j && array[i] <= pivot) {
+                ++i;
+            }
+            while (i <= j && array[j] > pivot) {
+                --j;
+            }
+
+            if (i < j) {
+                int temp = array[i];
+                array[i] = array[j];
+                array[j] = temp;
+            }
+        }
+        int temp = array[left];
+        array[left] = array[j];
+        array[j] = temp;
+
+        return j;
+    }
+
+    /**
+     * Uses a quick sort algorithm with a Median of Medians pivot 
+     * to find the kth smallest entry in an array
+     * @param array the array to be operated on
+     * @param entry the index of the item to be returned
+     * @return the kth item
+     */
+    public static int quickSelectMM(int[] array, int entry) {
+        return quickSelectMM(array, 0, array.length - 1, entry-1);
+    }
+
+    private static int quickSelectMM(int[] array, int left, int right, int entry) {
+        if (left == right)
+            return array[left];
+        
+        int pivotIndex = medianOfMedians(array, left, right);
+
+        pivotIndex = partitionMM(array, left, right, pivotIndex);
+
+        if (entry == pivotIndex)
+            return array[entry];
+        else if (entry < pivotIndex)
+            return quickSelect(array, left, pivotIndex -1, entry);
+        else
+            return quickSelect(array, pivotIndex + 1, right, entry);
+    }
+
+    private static int medianOfMedians(int[] array, int left, int right) {
+        if (right - left < 5)
+            return partitionFive(array, left, right);
+
+        int medianCount = (int) Math.ceil((double)(right - left + 1) / 5);
+        int[] medians = new int[medianCount];
+
+        for (int i = 0; i < (medianCount - 1); ++i) {
+            int medianLeft = left + i * 5;
+            int medianRight = Math.min(left + i * 5 + 4, right);
+            medians[i] = medianOfMedians(array, medianLeft, medianRight);
+        }
+
+        return medianOfMedians(medians, 0, medianCount - 1);
+    }
+
+    private static int partitionFive(int[] array, int left, int right) {
+        Arrays.sort(array, left, right + 1);
+        return left + (right - left) / 2;
+    }
+
+    private static int partitionMM(int[] array, int left, int right, int pivotIndex) {
+        int pivot = array[pivotIndex];
+        // swap item at pivotIndex with item at right
+        int temp = array[pivotIndex];
+        array[pivotIndex] = array[right];
+        array[right] = temp;
+
+        int newPivotIndex = left;
+
+        for (int i = left; i < right; ++i) {
+            if (array[i] < pivot) {
+                // swap items
+                temp = array[i];
+                array[i] = array[newPivotIndex];
+                array[newPivotIndex] = temp;
+                // incremenet 
+                ++newPivotIndex;
+            }
+        }
+
+        temp = array[newPivotIndex];
+        array[newPivotIndex] = array[right];
+        array[right] = temp;
+        return newPivotIndex;
+    }
+
 }
